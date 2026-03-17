ad99a4db42b65760a82830f6544ef9fbddde27a0
Justix <jgallau1@gmail.com>
2026-03-17 19:43:26 +0100

    picture styling possible fix

--- src/datamanagment/data.tsx
+++ src/datamanagment/data.tsx
@@ -1,25 +1,25 @@
 import data from "../data.json";
 
+const base = import.meta.env.BASE_URL;
+
 export function getProfilePicURL() {
-  return data.profilepicURL;
+  return `${base}${data.profilepicURL}`;
 }
-
 export function getBannerPicURL() {
-  return data.bannerURL;
+  return `${base}${data.bannerURL}`;
 }
-
 export function getDescription() {
   return data.description;
 }
-
 export function getTitle() {
   return data.title;
 }
-
 export function getTechnologies() {
   return data.technologies;
 }
-
 export function getProjects() {
-  return data.projects;
+  return data.projects.map(p => ({
+    ...p,
+    image: `${base}${p.image}`
+  }));
 }
